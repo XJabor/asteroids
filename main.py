52f5cdf6900274c9ff25326470eb55f2d0690e7a
XJabor <johnplaziak@protonmail.com>
2025-03-22 23:22:56 -0500

    Refactored codebase: Added circleshape.py, updated player.py, main.py, and constants.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -11,16 +12,29 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
+
+        # this is my movement controls
+        updateable.update(dt)
+
+        # draw everything
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         #limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
